2fb2578281821991d5e9640e6cd2c86291f15aa4
n0tch <gusoliv3ira@gmail.com>
2015-03-26 13:51:41 -0300

    Arquivo Comentado

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -5,20 +5,23 @@ class AnalysisController < ApplicationController
 
 	def create
 		@arquivo = params[:doc]
+		
 		@tipo = File.extname(@arquivo.original_filename)
-		# flash[:notice] = "Passou Aqui. #{@tipo}"
+		# File.exxtname extrai a extençao do arquivo.
+
 		if @tipo == '.exe'
 			flash[:notice] = "Impossivel Upar Arquivos Executaveis"
 		else
 			
 			@texto = []
 
-			File.open("/home/gustavo/web-app/analise/README.rdoc", "r").each do |infile|
-			    @texto << infile
+			File.open("/home/gustavo/web-app/analise/README.rdoc", "r").each do |file|
+						# ^ arquivo de teste que se encontra em disco.
+			    @texto << file
 			end
 
 			flash[:notice] = "Texto: #{@texto.to_s.split}"
-
+			# metodo split coloca cada pakavra em uma posiçao diferente em um array
 		end
 
 		redirect_to root_path
