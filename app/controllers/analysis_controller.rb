6a1ce60fa7fae1f18d385ed01dca5c291bca2519
n0tch <gusoliv3ira@gmail.com>
2015-03-28 15:28:38 -0300

    Controller contando as palavras e mostrando na view create.

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -7,14 +7,15 @@ class AnalysisController < ApplicationController
 		@arquivo = params[:doc]
 		
 		# @tipo = File.extname(@arquivo.original_filename)
-		# File.exxtname extrai a extençao do arquivo.
+		
 
 		if File.extname(@arquivo.original_filename) == '.exe'
+			# File.extname extrai a extençao do arquivo.
 			flash[:notice] = "Impossivel Upar Arquivos Executaveis"
 		else
 			@texto = []
 			# File.open(@arquivo.to_s, "r").map{|file| @texto << file }
-			IO.foreach("/home/gustavo/web-app/analise/teste.txt"){|x| @texto << x}
+			IO.foreach(Dir.pwd + "/Gemfile"){|x| @texto << x}
 
 			@array = @texto.to_s.split(" ")
 			# cria um array com uma posicao diferente para cada palavra
@@ -23,14 +24,24 @@ class AnalysisController < ApplicationController
 			
 			@array.each do |f|
 				# @outro << f.gsub(/[\\n]/, '')
-				@outro << f.gsub(/\\n/,"")
+				# f = f.gsub(/\\n/,"")
+				# f = f.delete "\""
+				@outro << f.gsub(/\\n/,"").delete("\"")
 				# metodo strip retira os '\n' da string
 				# metodo gsub retira os caracteres que sao passados como parametro
 			end
 			
-			flash[:notice] = @outro
+			@resultado = []
+
+			frequencies = Hash.new(0)
+			@outro.each { |word| frequencies[word] += 1 }
+			frequencies = frequencies.sort_by {|a, b| b }
+			frequencies.reverse!
+			frequencies.each { |word, frequency| @resultado << word + " - " + frequency.to_s }
+
+			# flash[:notice] = @resultado
 			# metodo split coloca cada pakavra em uma posiçao diferente em um array
 		end
-		redirect_to root_path
+		# redirect_to root_path
 	end
 end
